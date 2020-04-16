ce010550803587ef24e1486b1c339e59b929266d
wjamesTMC <jamesw@csps.com>
2020-04-16 17:13:53 -0400

    Finished corrections and file outputs

--- 1-CCC-base-code TEST_1.R
+++ 1-CCC-base-code TEST_1.R
@@ -1,6 +1,6 @@
 ##############################################################################
 #
-# CCC Sentiment Analysis 
+# CCC Sentiment Analysis - Vocabulary extraction and building
 # Bill James / jamesw@csps.com
 #
 # Files:  https://github.com/wjamesTMC/ccc-sentiment analysis.git
@@ -56,8 +56,9 @@ e_dat[e_dat == "Technical/Feature"]          <- "Technical"
 e_dat[e_dat == "Technical/Bug/System Issue"] <- "Technical"
 e_dat[e_dat == "Broken Link"]                <- "Technical"
 
-# Remove duplicates
+# Remove duplicates and blank descriptions, if any
 e_dat <- e_dat %>% filter(C_Type != "Duplicate", C_Type != "")
+e_dat <- e_dat %>% filter(Desc != "<None>")
 e_dat <- as.data.frame(e_dat)
 
 # Pull in Trillion Word list
@@ -65,41 +66,63 @@ base_url <- "https://programminghistorian.org/assets/basic-text-processing-in-r"
 url <- sprintf("%s/sotu_text/236.txt", base_url)
 wf <- read_csv(sprintf("%s/%s", base_url, "word_frequency.csv"))
 
-# Start with Junk problems
-cat_type <- "Junk or No Answer Required"
+# Establish the category list
+cat_list <- unique(e_dat$C_Type)
 
-# Create a dataframe to hold the results
-junk_df <- data.frame(cat_word = 1:500, cat_type = cat_type, cat_count = 1:500)
-
-# Pull all the junk from the data file
-junk <- e_dat %>% filter(C_Type == "Junk or No Answer Required")
-
-# Loop through Junk and build a dataframe of the results
-new_df_line <- data.frame(cat_word = 1, cat_type = cat_type, cat_count = 1)
-for(i in 1:nrow(junk)) {
-     text  <- junk[i,3]
-     words <- tokenize_words(text)
-     tab <- table(words)
-     tab <- data_frame(word = names(tab), count = as.numeric(tab))
-     tab <- arrange(tab, desc(count))
-     tab <- inner_join(tab, wf)
-     wds <- filter(tab, frequency < 0.001)
-     for(j in 1:nrow(wds)) {
-          new_df_line[1,1] <- wds[j,1]
-          new_df_line[1,3] <- wds[j,2]
-          junk_df <- rbind(junk_df, new_df_line)
+# Outer loop to build a vocab list for each category
+for(x in 1:length(cat_list)) {
+     
+     # Establish the category for the inner loop
+     cat_name <- cat_list[x]
+     
+     # Pull all the rows for that category from the data file
+     category <- e_dat %>% filter(C_Type == cat_name)
+     cat_len <- nrow(category)
+     
+     # Create a dataframe to hold the results
+     cat_df <- data.frame(cat_word = 1:cat_len, cat_name = cat_name, cat_count = 1:cat_len)
+     
+     # Loop through the category and build a dataframe of the results
+     new_df_line <- data.frame(cat_word = 1, cat_name = cat_name, cat_count = 1)
+     
+     # Now the inner loop to generate the results
+     for(i in 1:cat_len) {
+          text  <- category[i,3]
+          words <- tokenize_words(text)
+          tab <- table(words)
+          tab <- data_frame(word = names(tab), count = as.numeric(tab))
+          tab <- arrange(tab, desc(count))
+          tab <- inner_join(tab, wf)
+          wds <- filter(tab, frequency < 0.001)
+          if(nrow(wds) > 0) {
+               for(j in 1:nrow(wds)) {
+                    new_df_line[1,1] <- wds[j,1]
+                    new_df_line[1,3] <- wds[j,2]
+                    cat_df <- rbind(cat_df, new_df_line)
+               }
+          }
      }
+     # Remove unused rows
+     cat_df <- cat_df %>% filter(cat_word != cat_count)
+     cat_df <- arrange(cat_df, desc(cat_count))
+     
+     # Remove duplicates (some emails can appear twice)
+     cat_df <- unique(cat_df)
+     
+     # Limit lines for writing - Google Sheets limits to 1000 lines
+     if(nrow(cat_df) > 1000) {
+          cat_df <- cat_df %>% filter(cat_count > 2)
+     }
+     
+     # Write out the results
+     output_file_name <- paste(x, " - ", cat_name)
+     
+     output_file <- gs_new(output_file_name)
+     gs_edit_cells(output_file, ws = 1, input = cat_df, anchor = "A1", byrow = FALSE,
+                   col_names = NULL, trim = FALSE, verbose = TRUE)
 }
 
-# Remove unused rows
-junk_df <- junk_df %>% filter(cat_word != cat_count)
-junk_df <- arrange(junk_df, desc(cat_count))
-
-# Remove duplicates (some emails can appear twice)
-junk_df <- unique(junk_df)
-
-# Add to the summary dataframe
-summary_df <- rbind(summary_df, junk_df)
+# END OF CURRENT CODING EXPERIMENT - NEED TO DETERMINE UNIQUE VOCAB BY CATEGORY
 
 #
 # Vocabulary file - the list of terms we are using to predict classifications
